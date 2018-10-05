4ab8ea8cc0b1ceb482bac54c364ec3ebe6ea0532
Robert <justdoit2016@aliyun.com>
2018-10-05 20:14:22 +0800

    svg代码整理到icon.vue

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,6 @@
 <template>
 <button class="g-button" :class="`icon-${iconPosition}`">
-       <svg v-if="icon" class="icon">
-                <use :xlink:href="`#i-${icon}`"></use>
-            </svg>
+      <g-icon v-if="icon" :name="icon"></g-icon>
             <div class="content">
        <slot></slot>
             </div>
@@ -11,16 +9,21 @@
 
 <script>
 export default {
-  props: ["icon", "iconPosition"]
+  // props: ["icon", "iconPosition"]
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(value) {
+        return value == "left" || value == "right";
+      }
+    }
+  }
 };
 </script>
 
 <style lang="scss">
-.icon {
-  width: 1em;
-  height: 1em;
-}
-
 .g-button {
   height: var(--button-height);
   line-height: var(--line-height);
